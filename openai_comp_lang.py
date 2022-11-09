fbc2f123f378e43b48f7abdd6d6dd612a2aecdb5
LSaldyt <lucassaldyt@gmail.com>
2022-11-08 16:42:42 -0800

    Composition and enumeration

--- openai_comp_lang.py
+++ openai_comp_lang.py
@@ -12,7 +12,7 @@ Element = namedtuple('Element', ['description', 'rule'])
 
 synthetic_grammar = dict(
     remove_all=Element(
-        "remove all instances of the letter e from the string"
+        "remove all instances of the letter e from the string",
         lambda s : s.replace("e", "")),
     replace_spaces=Element(
         "replace all spaces with exclamation points in the string",
@@ -51,85 +51,27 @@ synthetic_grammar = dict(
         "delete all excamation points, question marks, and periods from the string",
         lambda s : "".join([x for x in s if x not in ".!?"])))
 
-'''
-This synthetic task is arbitrary, and a lot of the operators could be generalized into a grammar
+''' This synthetic task is arbitrary, and a lot of the operators could be generalized into a grammar '''
 
-'''
+def enumerate(depth=3):
+    ''' Generate without pruning? '''
+    depth -= 1
+    generated = [[el] for el in synthetic_grammar.values()] # depth 1
+    for i in range(depth - 1):
+        generated += [created for created in [compose(el + [new_el])
+                      for new_el in synthetic_grammar.values()]
+                      for el in generated]
+    return generated
 
-from collections import namedtuple
-
-StringTree = namedtuple('StringTree', ['content', 'name', 'child'])
-
-class StringLanguage:
-    def __init__(self):
-        self.terminals = 'abc'
-        self.nums      = [2, 3, 4]
-        self.rules = [self.repeat, self.group]
-
-    def repeat(self, expr):
-        for num in self.nums:
-            yield StringTree(num, 'repeat', expr)
-
-    def group(self, expr):
-        for term in self.terminals:
-            yield StringTree(term, 'group', expr)
-
-    def initial(self):
-        for term in self.terminals:
-            yield StringTree(term, 'terminal', None)
-
-    def grow(self, programs):
-        for program in programs:
-            yield program
-            for rule in self.rules:
-                yield from rule(program)
-
-    def interpret(self, node):
-        match node.name:
-            case 'terminal':
-                return node.content
-            case 'repeat':
-                return node.content * self.interpret(node.child)
-            case 'group':
-                return node.content + self.interpret(node.child)
-
-    def render(self, node):
-        match node.name:
-            case 'terminal':
-                return node.content
-            case 'repeat':
-                return f'{node.content}({self.render(node.child)})'
-            case 'group':
-                return f'{node.content}({self.render(node.child)})'
-
-def filter_observationally_equivalent(language, programs, inputs):
-    seen = set()
-    for program in programs:
-        output = language.interpret(program)
-        if output not in seen:
-            seen.add(output)
-            yield program
-
-def bottom_up_explicit(language, inputs, outputs):
-    programs = list(language.initial())
-    while True:
-        programs.extend(list(language.grow(programs)))
-        programs = list(filter_observationally_equivalent(language, programs, inputs))
-        for program in programs:
-            if language.interpret(program) == outputs:
-                return program
+def compose(elements, connective='then'):
+    ''' [Element] -> Element '''
+    a, b = elements
+    return Element(a.description + ' ' + connective + ' ' + b.description,
+                   lambda s : a.rule(b.rule(s)))
 
 def main():
-    language = StringLanguage()
-    # programs = list(language.initial())
-    # for i in range(3):
-    #     for program in programs:
-    #         print(language.render(program))
-    #     programs.extend(list(language.grow(programs)))
-    # target = 'aaa'
-    target = 'ababab'
-    result = bottom_up_explicit(language, '', target)
-    print(language.render(result), ' -> ', target)
+    for el in enumerate(depth=2):
+        print(el)
 
 if __name__ == '__main__':
     main()
